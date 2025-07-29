4133e3c5f83aa26a38f7f2b0c752d87b0f80905c
Yannick <yannicksookree@gmail.com>
2025-07-29 15:03:53 +0400

    _putchar

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int _puts(char *str);
+
+#endif /* MAIN_H */
